bb0f9a11fa188553b369ded2f3c615666d9f7e05
PhilippSchwarzePM <philipps@perfectomobile.com>
2017-03-21 13:02:10 +0100

    Initial Commit

---
+++ Insurance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insurance
+//
+//  Created by Philipp Schwarze on 21/03/2017.
+//  Copyright © 2017 Philipp Schwarze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
