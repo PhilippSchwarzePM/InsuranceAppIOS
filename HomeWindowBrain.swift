465f87667d7b66a62fc2b832864689425159b2eb
PhilippSchwarzePM <philipps@perfectomobile.com>
2017-03-22 18:46:04 +0100

    first commit

---
+++ HomeWindowBrain.swift
@@ -0,0 +1,9 @@
+//
+//  HomeWindowBrain.swift
+//  Insurance
+//
+//  Created by Philipp Schwarze on 22/03/2017.
+//  Copyright © 2017 Philipp Schwarze. All rights reserved.
+//
+
+import Foundation
